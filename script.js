ce0bbf5c17c860451fd50538590b7706eebd2685
wdnmd1253437 <104597511+wdnmd1253437@users.noreply.github.com>
2022-04-28 14:03:43 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,6 @@
+$("button").click(function(){
+     let variable1 = $(".A").val();
+     let variable2 = $(".B").val();
+      let variable3 = $(".C").val();
+     console.log (variable1,variable2, variable3);
+});
